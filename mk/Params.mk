157a266456088943db6747de11ac32b56ee84d71
Florian Marrero Liestmann <f.m.liestmann@fx-ttr.de>
2024-07-29 19:26:44 +0200

    Add main.c; add C+LFLAGS

--- mk/Params.mk
+++ mk/Params.mk
@@ -7,6 +7,9 @@ endef
 # General build properties
 IMAGE_NAME = mu4
 ARCH = amd64
+CFLAGS = -fno-pic -static -fno-builtin -fno-strict-aliasing -Wall -MD -ggdb -fno-omit-frame-pointer -fno-stack-protector -ffreestanding -fno-common -nostdlib -Iinclude -gdwarf-2 -m64 -DX64 -mcmodel=kernel -mtls-direct-seg-refs -mno-red-zone
+ASFLAGS = -fno-pic -gdwarf-2 -Wa,-divide -Iinclude -m64 -DX64 -mcmodel=kernel -mtls-direct-seg-refs -mno-red-zone
+LDFLAGS = -m elf_x86_64 -z nodefaultlib
 
 # Toolchain properties
 TOOLCHAIN = # Set this option if you want to use a custom toolchain (for cross compilation for example)
